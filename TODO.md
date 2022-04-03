c50ec5998725c7b41d8f71afa1f517afd9232964
Gabriel Birke <gabriel.birke@gmail.com>
2022-04-03 17:43:00 +0200

    Format code with prettier

--- TODO.md
+++ TODO.md
@@ -1,12 +1,12 @@
 ## Next steps
 
-- [X] Replace starter-kit with my subset of features: headings, lists,
-	paragraphs, links, hr, bold, italic, blockquote
-- [ ] Add prettier
+- [x] Replace starter-kit with my subset of features: headings, lists,
+      paragraphs, links, hr, bold, italic, blockquote
+- [X] Add prettier
 - [ ] Try out mentions:
   - Can they be made clickable?
   - Can they become `a` instead of `span`, with href?
 - [ ] Test driven development of Prosemirror-JSON-to-markdown renderer and
-	JSON-to-Markdown-to-HTML-to-JSON roundtrip.
-	Check Prosemirror Markdown serializer for inspiration. Add Markdown-it
-	for rendering HTML again. Special rendering for mentions?
+      JSON-to-Markdown-to-HTML-to-JSON roundtrip.
+      Check Prosemirror Markdown serializer for inspiration. Add Markdown-it
+      for rendering HTML again. Special rendering for mentions?
--- src/main.ts
+++ src/main.ts
@@ -1,40 +1,47 @@
-import './style.css'
+import "./style.css";
 
-import { Editor } from '@tiptap/core'
-import Blockquote from '@tiptap/extension-blockquote'
-import Bold from '@tiptap/extension-bold'
-import BulletList from '@tiptap/extension-bullet-list'
-import Document from '@tiptap/extension-document'
-import Gapcursor from '@tiptap/extension-gapcursor'
-import Heading from '@tiptap/extension-heading'
-import History from '@tiptap/extension-history'
-import HorizontalRule from '@tiptap/extension-horizontal-rule'
-import Italic from '@tiptap/extension-italic'
-import ListItem from '@tiptap/extension-list-item'
-import OrderedList from '@tiptap/extension-ordered-list'
-import Paragraph from '@tiptap/extension-paragraph'
-import Text from '@tiptap/extension-text'
+import { Editor } from "@tiptap/core";
+import Blockquote from "@tiptap/extension-blockquote";
+import Bold from "@tiptap/extension-bold";
+import BulletList from "@tiptap/extension-bullet-list";
+import Document from "@tiptap/extension-document";
+import Gapcursor from "@tiptap/extension-gapcursor";
+import Heading from "@tiptap/extension-heading";
+import History from "@tiptap/extension-history";
+import HorizontalRule from "@tiptap/extension-horizontal-rule";
+import Italic from "@tiptap/extension-italic";
+import ListItem from "@tiptap/extension-list-item";
+import OrderedList from "@tiptap/extension-ordered-list";
+import Paragraph from "@tiptap/extension-paragraph";
+import Text from "@tiptap/extension-text";
 
 const editor = new Editor({
-  element: document.getElementById('myEditor')!,
+  element: document.getElementById("myEditor")!,
   extensions: [
-	  Blockquote,
-	  Bold,
-	  BulletList,
-	  Document,
-		Gapcursor,
-		Heading,
-		History,
-		HorizontalRule,
-		Italic,
-		ListItem,
-		OrderedList,
-		Paragraph,
-		Text
+    Blockquote,
+    Bold,
+    BulletList,
+    Document,
+    Gapcursor,
+    Heading,
+    History,
+    HorizontalRule,
+    Italic,
+    ListItem,
+    OrderedList,
+    Paragraph,
+    Text,
   ],
-  content: '<p>Hello World!</p><ul><li>First list item</li><li>Other item with <em>emphasis</em></li></ul><h1>A heading</h1><h2>A subheading</h2><blockquote>To err is human</blockquote><hr>',
-})
-
-document.getElementById('getEditorJson')?.addEventListener('click', function() {
-	document.getElementById('editorJsonDisplay')!.textContent = JSON.stringify( editor.getJSON(), null, 2)
+  content:
+    "<p>Hello World!</p><ul><li>First list item</li><li>Other item with <em>emphasis</em></li></ul><h1>A heading</h1><h2>A subheading</h2><blockquote>To err is human</blockquote><hr>",
 });
+
+document
+  .getElementById("getEditorJson")
+  ?.addEventListener("click", function () {
+    document.getElementById("editorJsonDisplay")!.textContent = JSON.stringify(
+      editor.getJSON(),
+      null,
+      2
+    );
+  });
