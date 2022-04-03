77a65f8805339fe30648ad4cd7320fe8568cba8e
Gabriel Birke <gabriel.birke@gmail.com>
2022-04-03 16:42:38 +0200

    Add JSON debug button

--- src/main.ts
+++ src/main.ts
@@ -3,10 +3,14 @@ import './style.css'
 import { Editor } from '@tiptap/core'
 import StarterKit from '@tiptap/starter-kit'
 
-new Editor({
+const editor = new Editor({
   element: document.getElementById('myEditor')!,
   extensions: [
     StarterKit,
   ],
   content: '<p>Hello World!</p><ul><li>First list item</li><li>Other item with <em>emphasis</em><li></ul><h1>A heading</h1><h2>A subheading</h2>',
 })
+
+document.getElementById('getEditorJson')?.addEventListener('click', function() {
+	document.getElementById('editorJsonDisplay')!.textContent = JSON.stringify( editor.getJSON(), null, 2)
+});
