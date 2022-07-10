6e97e45a8107770a433f7581e4de220459327cf6
Gabriel Birke <gabriel.birke@posteo.net>
2022-07-10 18:37:38 +0200

    Add MarkdownParserState
    Inspired by the proselint markdown extension, this state creates and
    tracks tiptap JSONContent objects instead of Prosemirror Nodes from
    a Prosemirror Schema.

---
+++ src/MarkdownParserState.ts
@@ -0,0 +1,100 @@
+import type { JSONContent } from "@tiptap/core";
+import type Token from "markdown-it/lib/token";
+
+// To pacify ESLint, in tiptap attrs are defined as "any"
+type JSONAttr = string|number|null;
+
+const createNode = (
+  type?: string,
+  attrs?: Record<string, JSONAttr>,
+  content?: JSONContent[]
+): JSONContent => {
+  if (!type) {
+    throw new Error("Type must not be empty");
+  }
+  const node: JSONContent = { type };
+  if (attrs) {
+    node.attrs = attrs;
+  }
+  if (content) {
+    node.content = content;
+  }
+  return node;
+};
+
+export class MarkdownParseState {
+  stack: JSONContent[];
+
+  constructor(
+    // TODO use markdown-it token definition
+    readonly tokenHandlers: {
+      [token: string]: (
+        stat: MarkdownParseState,
+        token: Token,
+        tokens: Token[],
+        i: number
+      ) => void;
+    }
+  ) {
+    this.stack = [{ type: "doc", content: [] }];
+  }
+
+  top() {
+    return this.stack[this.stack.length - 1];
+  }
+
+  push(elt: JSONContent) {
+    if (this.stack.length) {
+      const top = this.top();
+      if (!top.content) {
+        top.content = [];
+      }
+      top.content.push(elt);
+    }
+  }
+
+  // Adds the given text to the current position in the document,
+  // using the current marks as styling.
+  addText(text: string) {
+    if (!text) return;
+    const top = this.top();
+
+    if (!top.content) {
+      top.content = [];
+    }
+    // TODO merge text nodes with the same marks
+    const textNode = {
+      type: "text",
+      text,
+      marks: top.marks,
+    };
+    top.content.push(textNode);
+  }
+
+  // Add a node at the current position.
+  addNode(
+    type?: string,
+    attrs?: Record<string, JSONAttr>,
+    content?: JSONContent[]
+  ): JSONContent {
+    const node = createNode(type, attrs, content);
+    this.push(node);
+    return node;
+  }
+
+  // Wrap subsequent content in a node of the given type.
+  openNode(type?: string, attrs?: Record<string, JSONAttr>) {
+    this.stack.push(createNode(type, attrs));
+  }
+
+  // Close and return the node that is currently on top of the stack.
+  closeNode() {
+    const info = this.stack.pop();
+    if (info === undefined) {
+      throw Error(
+        "You must not call closeNode when stack is empty. Please check your logic."
+      );
+    }
+    return this.addNode(info.type, info.attrs, info.content);
+  }
+}
---
+++ test/MarkdownParserState.test.ts
@@ -0,0 +1,83 @@
+import { describe, expect, test } from "vitest";
+import type { JSONContent } from "@tiptap/core";
+import { MarkdownParseState } from "../src/MarkdownParserState";
+
+describe("MarkdownParseState", () => {
+  const getDocument = (state: MarkdownParseState): JSONContent => {
+    let doc: JSONContent;
+    do {
+      doc = state.closeNode();
+    } while (state.stack.length);
+    return doc;
+  };
+
+  test("empty state returns doc with no content", () => {
+    const state = new MarkdownParseState({});
+
+    expect(getDocument(state)).toEqual({ type: "doc", content: [] });
+  });
+
+  test("adding a node appends to document", () => {
+    const state = new MarkdownParseState({});
+
+    state.addNode("paragraph");
+
+    expect(getDocument(state)).toEqual({
+      type: "doc",
+      content: [{ type: "paragraph" }],
+    });
+  });
+
+  test("adding multiple node appends them to document", () => {
+    const state = new MarkdownParseState({});
+
+    state.addNode("paragraph");
+    state.addNode("paragraph");
+
+    expect(getDocument(state)).toEqual({
+      type: "doc",
+      content: [{ type: "paragraph" }, { type: "paragraph" }],
+    });
+  });
+
+  test("opening and closing nodes nests them", () => {
+    const state = new MarkdownParseState({});
+
+    state.openNode("bulletList");
+    state.openNode("listItem");
+    state.openNode("paragraph");
+    state.closeNode(); // paragraph
+    state.closeNode(); // listItem
+    state.closeNode(); // bulletList
+    state.openNode("paragraph");
+
+    expect(getDocument(state)).toEqual({
+      type: "doc",
+      content: [
+        {
+          type: "bulletList",
+          content: [{ type: "listItem", content: [{ type: "paragraph" }] }],
+        },
+        { type: "paragraph" },
+      ],
+    });
+  });
+
+    test("adding text adds it to last opened node", () => {
+    const state = new MarkdownParseState({});
+
+    state.openNode("paragraph");
+    state.addText("first para");
+    state.closeNode();
+    state.openNode("paragraph");
+    state.addText("second para");
+
+    expect(getDocument(state)).toEqual({
+      type: "doc",
+      content: [
+          { type: "paragraph", content: [{type: "text", text:"first para"}] },
+          { type: "paragraph", content: [{type: "text", text:"second para"}] }],
+    });
+  });
+
+});
